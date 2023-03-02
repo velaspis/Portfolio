9bf096f616c52a77380cb91db009165f934e2d1d
velaspis <91197854+velaspis@users.noreply.github.com>
2023-03-02 11:49:14 +0100

    navbar fix final

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -1,34 +1,65 @@
-import React, { useState } from "react";
-import useMediaQuery from "../hooks/useMediaQuery";
+import React from "react";
+import { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+        hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
 
 const Navbar = ({ selectedPage, setSelectedPage }) => {
-  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const [menuToggled, setMenuToggled] = useState(false);
   const isAboveSmallscreens = useMediaQuery("(min-width:760px)");
   return (
-    <nav className={`z-40 w-full fixed top-0 py-6`}>
+    <nav className={` z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">VE</h4>
-
         {/*Desktop nav*/}
 
         {isAboveSmallscreens ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
-            <AnchorLink
-              className={`${
-                selectedPage === "home" ? "text-yellow" : "text-white"
-              }
-                  hover:text-yellow transition duration-500`}
-              href="#home"
-              onClick={() => {
-                setSelectedPage("home");
-              }}
-            >
-              Home
-            </AnchorLink>
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Testimonials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
           </div>
         ) : (
-          <div></div>
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setMenuToggled(!menuToggled)}
+          >
+            <img alt="menu-icon" src="../assets/menu-icon.svg" />
+          </button>
         )}
       </div>
     </nav>
