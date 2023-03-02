fdfc8ebc3705534213137567e2cb61ca31ced990
velaspis <91197854+velaspis@users.noreply.github.com>
2023-03-02 13:18:26 +0100

    dot group and navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import Navbar from "./scenes/Navbar.jsx";
-import { useState } from "react";
-import useMediaQuery from "./hooks/useMediaQuery";
+import React, { useState, useEffect } from "react";
 
+import useMediaQuery from "./hooks/useMediaQuery";
+import DotGroup from "./scenes/DotGroup";
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
   //determines what page ur on
- const isAboveMedium = useMediaQuery("(min-width: 1060px)");
+  const isAboveMedium = useMediaQuery("(min-width: 1060px)");
   //determines if ur on a medium screen or not
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage("home");
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-deep-blue">
-      <Navbar SelectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        SelectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMedium && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          ></DotGroup>
+        )}
+      </div>
     </div>
   );
 }
--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -16,11 +16,12 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }) => {
+const Navbar = ({ isTopOfPage,selectedPage, setSelectedPage }) => {
   const [menuToggled, setMenuToggled] = useState(false);
   const isAboveSmallscreens = useMediaQuery("(min-width:760px)");
+  const navbarBackground = isTopOfPage ? "" : "bg-red";
   return (
-    <nav className={` z-40 w-full fixed top-0 py-6`}>
+    <nav className={`${navbarBackground}z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">VE</h4>
         {/*Desktop nav*/}
@@ -58,8 +59,48 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
             className="rounded-full bg-red p-2"
             onClick={() => setMenuToggled(!menuToggled)}
           >
-            <img alt="menu-icon" src="../assets/menu-icon.svg" />
+            <img alt="" src="./assets/menu-icon.svg" />
           </button>
+        )}
+
+        {/*button popup*/}
+        {!isAboveSmallscreens && menuToggled && (
+          <div className="fixed bottom-0 right-0 w-[300px] h-full bg-red">
+            {/*close icon */}
+            <div className="flex justify-end p-">
+              <button onClick={() => setMenuToggled(!menuToggled)}>
+                <img alt="close" src="./assets/close-icon.svg" />
+              </button>
+            </div>
+            {/*links*/}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
         )}
       </div>
     </nav>
